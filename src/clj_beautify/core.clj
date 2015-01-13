3016e6d32d8d5cba7fdd312a8d855a4edfeb16fa
Mitch Comardo <mcomardo@pros.com>
2015-01-13 13:27:47 -0600

    file discovery based on the passed in args to executable.

--- src/clj_beautify/core.clj
+++ src/clj_beautify/core.clj
@@ -1,7 +1,17 @@
 (ns clj-beautify.core
+  (:require [clj-beautify.file-handler :refer [list-files]])
   (:gen-class))
 
 (defn -main
-  "I don't do a whole lot ... yet."
+  "Entry point of the command line program.
+   (0 args) -> represents the mode to open the program in (clj or edn)
+   (1 args) -> represents the file or directory to walk for formatting"
   [& args]
-  (println "Hello, World!"))
+  (println args)
+  (let [arg-cnt (count args)]
+    (when (< 2 arg-cnt)
+      (throw (Exception. (str "Invalid number of arguements. Expected 2 but "
+                              "found " arg-cnt))))
+    ;; TODO: do something with the valid args
+    (let [files (list-files (nth args 1))]
+      (println files))))
--- src/clj_beautify/file_handler.clj
+++ src/clj_beautify/file_handler.clj
@@ -1,4 +1,5 @@
-(ns clj-beautify.file-handler)
+(ns clj-beautify.file-handler
+  (:require [clojure.java.io :refer [file]]))
 
 (defn read-file
   "Reads an entire file as a string"
@@ -9,3 +10,26 @@
   "Writes a newly formatted file"
   [filename contents]
   (spit filename contents))
+
+(defn match-clj
+  [f]
+  (let [filename (.getCanonicalPath f)]
+    (when (re-find #"\.clj" filename) filename)))
+
+(defn exists?
+  [f]
+  (.exists f))
+
+(defn list-files
+  [pth]
+  (let [f (file pth)]
+    (when-not (exists? f)
+      (throw (Exception. (str "File not found! " pth))))
+   (loop [files (file-seq f)
+          results ()]
+          (if (empty? files)
+            ;; then
+            (remove nil? results)
+            ;; else
+            (let [tmp (conj results (match-clj (first files)))]
+              (recur (rest files) tmp))))))
